b562072da2647bc06c8307279ce2594f31d972af
rivertw777 <105557972+rivertw777@users.noreply.github.com>
2024-08-12 17:09:22 +0900

    add gateway

---
+++ com.sparta.msa_exam.gateway/src/main/java/com/sparta/msa_exam/gateway/JwtAuthenticationFilter.java
@@ -0,0 +1,70 @@
+package com.sparta.msa_exam.gateway;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import javax.crypto.SecretKey;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Component
+public class JwtAuthenticationFilter implements GlobalFilter {
+
+    @Value("${service.jwt.secret-key}")
+    private String secretKey;
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String path = exchange.getRequest().getURI().getPath();
+        if (path.equals("/auth/signIn") || path.equals("/auth/signUp")) {
+            return chain.filter(exchange);
+        }
+
+        String token = extractToken(exchange);
+
+        if (token == null || !validateToken(token, exchange)) {
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+
+        return chain.filter(exchange);
+    }
+
+    private String extractToken(ServerWebExchange exchange) {
+        String authHeader = exchange.getRequest().getHeaders().getFirst("Authorization");
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            return authHeader.substring(7);
+        }
+        return null;
+    }
+
+    private boolean validateToken(String token, ServerWebExchange exchange) {
+        try {
+            SecretKey key = Keys.hmacShaKeyFor(Decoders.BASE64URL.decode(secretKey));
+            Jws<Claims> claimsJws = Jwts.parser()
+                    .verifyWith(key)
+                    .build().parseSignedClaims(token);
+            log.info("#####payload :: " + claimsJws.getPayload().toString());
+            Claims claims = claimsJws.getBody();
+
+            // 헤더 설정
+            exchange.getRequest().mutate()
+                    .header("X-User-Id", claims.get("user_id").toString())
+                    .header("X-Role", claims.get("role").toString())
+                    .build();
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+}
---
+++ com.sparta.msa_exam.gateway/src/main/java/com/sparta/msa_exam/gateway/PreFilter.java
@@ -0,0 +1,28 @@
+package com.sparta.msa_exam.gateway;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Component
+public class PreFilter implements GlobalFilter, Ordered {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+        log.info("Pre Filter: Request URI is " + request.getURI());
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return Ordered.HIGHEST_PRECEDENCE;
+    }
+}
